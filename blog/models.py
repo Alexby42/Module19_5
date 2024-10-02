5c2812753909d6dab125a0dc4bd9f7620665cc07
Alexby42 <163275700+Alexby42@users.noreply.github.com>
2024-10-02 22:33:11 +0700

    Postgre

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,28 @@
 from django.db import models
+# Create your models here.
 
-# Create your models here.
-# Create your models here.
+class Buyer(models.Model):
+    objects = None
+    name = models.CharField(max_length=100)
+    balance = models.DecimalField(decimal_places=2, max_digits=6)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Game(models.Model):
+    objects = None
+    title = models.CharField(max_length=200)
+    cost = models.DecimalField(decimal_places=2, max_digits=6)
+    size = models.DecimalField(decimal_places=2, max_digits=10)
+    description = models.TextField()
+    age_limited = models.BooleanField(default=False)
+    buyer = models.ManyToManyField(Buyer, related_name='title')
+
+    def __str__(self):
+        return self.title
+
 class Posts(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField(default='')
@@ -9,3 +30,10 @@ class Posts(models.Model):
 
     def __str__(self):
         return self.title
+
+class User(models.Model):
+    username = models.CharField(max_length=10)
+    age = models.IntegerField()
+
+    def __str__(self):
+        return self.username
